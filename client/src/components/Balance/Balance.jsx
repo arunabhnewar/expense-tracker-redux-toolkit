65d06c179a4d3f4f97091e835a12b34ccc540c33
arunabhnewar <newardevil@gmail.com>
2023-04-09 20:12:06 +0600

    calculate transactions

--- client/src/components/Balance/Balance.jsx
+++ client/src/components/Balance/Balance.jsx
@@ -1,12 +1,36 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import numberWithCommas from "../../utilities/numberWithCommas";
 
 const Balance = () => {
+  // use selector
+  const { transactions } = useSelector(state => state.transaction);
+
+  // income calculation
+  const calculateIncome = transactions => {
+    let income = 0;
+    transactions.forEach(transaction => {
+      const { type, amount } = transaction;
+      if (type === "income") {
+        income += amount;
+      } else {
+        income -= amount;
+      }
+    });
+
+    return income;
+  };
+
   return (
     <div className='top_card'>
       <p>Your Current Balance</p>
       <h3>
-        <span>৳</span>
-        <span>10500</span>
+        <span>৳</span>{" "}
+        {transactions?.length > 0 ? (
+          <span>{numberWithCommas(calculateIncome(transactions))}</span>
+        ) : (
+          0
+        )}
       </h3>
     </div>
   );
